9a3c44b57b6f2b1c52c9fe98d27c2bed6736e432
Yuliia Ziubrytska <yuliia.ziubrytska@recognyte.com>
2023-03-06 11:36:25 +0000

    Use dig as a container

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.19
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v5 v5.3.0
 	github.com/labstack/echo/v4 v4.10.2
+	github.com/pkg/errors v0.9.1
 	go.uber.org/dig v1.16.1
 	gorm.io/driver/postgres v1.4.8
 	gorm.io/gorm v1.24.6
@@ -15,16 +18,16 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
+	github.com/lib/pq v1.10.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
 	"go.uber.org/dig"
+	"ledger/internal/adapters"
 	"ledger/internal/common"
 	"ledger/internal/handlers"
+	"ledger/internal/logic"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
-
-type App struct {
-	container *dig.Container
-}
-
-func NewApp() App {
-	return App{container: dig.New()}
-}
 
 func main() {
 	e := echo.New()
@@ -22,19 +23,54 @@ func main() {
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
 
-	api := handlers.NewPublicAPI()
-	e.POST("/users/:uid/add", api.AddMoney)
-	e.POST("/users/:uid/balance", api.Balance)
-	e.POST("/users/:uid/history", api.History)
-
-	e.Logger.Fatal(e.Start(":8080")) //:TODO: Add graceful shutdown
-}
-
-func build(c *dig.Container) error {
+	c := dig.New()
 	if err := c.Provide(common.NewConfig); err != nil {
-		return err
+		log.Fatal(err)
+	}
+	if err := c.Provide(adapters.NewDB); err != nil {
+		log.Fatal(err)
 	}
 
-	return nil
+	if err := c.Provide(adapters.NewRepository); err != nil {
+		log.Fatal(err)
+	}
 
+	if err := c.Provide(logic.NewService); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Provide(handlers.NewPublicAPI); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Provide(func(config *common.Config) (logrus.Level, error) {
+		return logrus.ParseLevel(config.LogLevel)
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Provide(handlers.NewLogrusEntry); err != nil {
+		log.Fatal(err)
+	}
+
+	err := c.Invoke(func(api *handlers.PublicAPI) {
+		e.POST("/users/:uid/add", api.AddMoney)
+		e.POST("/users/:uid/balance", api.Balance)
+		e.POST("/users/:uid/history", api.History)
+		if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
